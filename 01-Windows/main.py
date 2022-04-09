b34faadeff5ab81d4ff21e87f6ab3b27b3a34028
jonas-lucas-duarte <jonaslucasduarte.github@gmail.com>
2022-04-09 15:18:05 -0300

    Modified 01-Windows/main.py

--- 01-Windows/main.py
+++ 01-Windows/main.py
@@ -4,9 +4,12 @@ from tkinter import *
 # windows = serves as a container to hold or contain these widgets
 
 window = Tk() # instantiate an instance of a window
+window.geometry("420x420")
+window.title("Bro Code first GUI program")
 
-window.geometry('420x420')
-
-window.title('Bro Code first GUI program')
+icon = PhotoImage(file='python.png')
+window.iconphoto(True, icon)
+window.config(background="black")
+window.config(background="#5cfcff")
 
 window.mainloop() # place window on computer screen, listen for events
